b5fcecd2a9e8ab3fd6ab59b9da6d29cb31bc0814
iwaki0710 <iwali.s.24kdgn@gmail.com>
2025-05-31 23:04:29 +0900

    5/31+

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,3 +1,5 @@
+const { resolveModuleName } = require("typescript");
+
 async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
@@ -6,11 +8,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() =>{
+      //ここでmap()関数を使ってuserListを加工している
+      const updatedList = userList.map(user => {
+        // return{full_name:'${user.family_name}${user.first_name}'};
+        user.full_name = `${user.family_name} ${user.first_name}`;
+        return user;
+      });
+      resolve(updatedList);
+    }, 3000);
+  });
 }
-
